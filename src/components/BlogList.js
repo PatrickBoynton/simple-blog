56cafde4e8571ac8f6ae40da9d2147300b0f8d37
PatrickBoynton <jpboynton3@gmail.com>
2021-02-10 13:13:58 -0500

    Set up the boilerplate for the post.

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -2,7 +2,12 @@ import { Component } from 'react';
 
 class BlogList extends Component {
     render() {
-        return <p>Blog List!</p>
+        return <ul>
+            <li>
+                <h1>This is a blog Title</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum iste nisi a itaque eligendi modi incidunt nobis nesciunt eveniet similique ex error, non, dolor consectetur consequuntur dignissimos est odit dolores.</p>
+            </li>
+        </ul>
     }
 }
 
