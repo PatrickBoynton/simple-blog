44ee73d39f56c5871c69ffeb846a18a5dd0c4347
PatrickBoynton <jpboynton3@gmail.com>
2021-02-10 15:36:14 -0500

    Changed the design of the form.

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,19 +4,27 @@ class Form extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            title: "",
-            text: "",
+            blogs: []
         }
         this.handleInput = this.handleInput.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-    
+
+    handleInput(event) {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    handleSubmit(event) {
+        this.props.addPost(this.state);
+        this.setState({title: "", text: ""});
+        event.preventDefault();
+    }
     render() {
-        return <form>
+        return <form onSubmit={this.handleSubmit}>
                 <label htmlFor="blog-title">Blog Title</label>
-                <input type="text" id="blog-title" name="blog-title"/>
-                <lable htmlFor="blog-text">Blog Text</lable>
-                <textarea id="blog-text" name="blog-text"></textarea>
+                <input type="text" id="blog-title" name="blog-title" onChange={this.handleInput}/>
+                <label htmlFor="blog-text">Blog Text</label>
+                <textarea id="blog-text" name="blog-text" onChange={this.handleInput}></textarea>
                 <button type="submit" className="btn">Create Post!</button>
             </form>
     }
