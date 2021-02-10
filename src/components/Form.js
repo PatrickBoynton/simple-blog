ae09ba1bc3bf5af7acc5d337d637d8dc1e6736c1
PatrickBoynton <jpboynton3@gmail.com>
2021-02-10 13:28:44 -0500

    Created the ctor.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,23 @@
 import { Component } from 'react';
 
 class Form extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "",
+            text: "",
+        }
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
     render() {
         return <form>
                 <label htmlFor="blog-title">Blog Title</label>
                 <input type="text" id="blog-title" name="blog-title"/>
                 <lable htmlFor="blog-text">Blog Text</lable>
                 <textarea id="blog-text" name="blog-text"></textarea>
-                <button className="btn">Create Post!</button>
+                <button type="submit" className="btn">Create Post!</button>
             </form>
     }
 }
