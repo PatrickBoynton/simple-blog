2cb94dd4f2062367a201b5431dc0662bb74c72f8
PatrickBoynton <jpboynton3@gmail.com>
2021-02-10 14:57:33 -0500

    Filled out the app logic.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,36 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.addPost = this.addPost.bind(this);
+  }
+
+  componentDidMount() {
+    const blogs = [
+      {
+        title: "This is a fun tile!",
+        text: "This is from the array!"
+      },
+      {
+        title: "This is a second tile!",
+        text: "This is a second blog post!"
+      }
+    ]
+    this.setState({blogs});
+  }
+
+  addPost(blog) {
+    const blogs = [...this.state.blogs];
+    localStorage.setItem("blogs", blogs);
+
+    this.setState({blogs})
+  }
   render() {
     return (
       <div className="App">
-        <Form/>
-        <BlogList/>
+        <Form addPost={this.addPost}/>
+        <BlogList blogs={this.state?.blogs}/>
       </div>
     );
   }
